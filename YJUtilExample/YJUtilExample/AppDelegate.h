a8e70bdf515816c51b1e0dcf7eb6a4c0913064d8
liuyujiao <liuyujiao_love1@sina.com>
2018-10-22 15:06:33 +0800

    util

---
+++ YJUtilExample/YJUtilExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJUtilExample
+//
+//  Created by 刘玉娇 on 2018/10/22.
+//  Copyright © 2018年 yunli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YJUtilExample/YJUtilExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJUtilExample
+//
+//  Created by 刘玉娇 on 2018/10/22.
+//  Copyright © 2018年 yunli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
